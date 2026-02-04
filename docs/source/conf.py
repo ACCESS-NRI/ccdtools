b265825fd04c30fc23d1593201be03a12409d845
Lawrence Bird <lawrence.bird@anu.edu.au>
2026-02-04 15:05:55 +1100

    Revert theme to sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,14 +9,15 @@
 # Imports
 import os
 import sys
+from datetime import datetime
 
 # Add the source code directory to sys.path
 sys.path.insert(0, os.path.abspath('../src'))
 
 # Define project information
 project = 'access-cryosphere-data-pool'
-copyright = '2026, ACCESS-NRI'
 author = 'ACCESS-NRI'
+copyright = f"{datetime.now().year}, {author}"
 release = 'v0.0.1dev5'
 
 # -- General configuration ---------------------------------------------------
@@ -66,21 +67,14 @@ suppress_warnings = ["autodoc.import_object", "autodoc", "docutils"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_book_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
+html_logo = "_static/ACCESS_Logo_2024_(RGB).png"
 html_theme_options = {
-    "use_edit_page_button": True,
     "github_url": "https://github.com/ACCESS-NRI/access-cryosphere-data-pool",
     "logo": {
         "image_light": "_static/ACCESS_Logo_2024_(RGB).png",
         "image_dark": "_static/ACCESS_Logo_2024_Dark_Background_(RGB).png",
     },
-}
-
-html_context = {
-    "github_user": "ACCESS-NRI",
-    "github_repo": "access-cryosphere-data-pool",
-    "github_version": "main",
-    "doc_path": "./docs",
 }
